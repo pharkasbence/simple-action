9537f5b125a27a93de3763a73dda3328e82a7a8a
Bence Farkas <bencefarkas89@gmail.com>
2024-02-21 12:31:59 +0100

    feat: add service provider

---
+++ src/Providers/SimpleActionServiceProvider.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace PharkasBence\SimpleAction\Providers;
+
+use Illuminate\Support\ServiceProvider;
+
+class SimpleActionServiceProvider extends ServiceProvider
+{
+    public function boot()
+    {
+        //
+    }
+
+    public function register()
+    {
+        //
+    }
+}
